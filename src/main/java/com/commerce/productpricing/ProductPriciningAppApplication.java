70c15a35a2b504793f5aa5a0a9100a9ac3958366
Fernando Salazar <e_fsalaz@mercadona.com>
2024-09-02 21:43:38 +0200

    feat/PPA-V0: Generated Spring Boot project and basic configuration

---
+++ src/main/java/com/commerce/productpricing/ProductPriciningAppApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.productpricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPriciningAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductPriciningAppApplication.class, args);
+    }
+
+}
